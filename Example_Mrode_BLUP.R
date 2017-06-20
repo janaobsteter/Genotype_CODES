6583bc58223c63ad1d44476f09b97628910fdbe1
jana.obsteter <obsteter.jana@gmail.com>
2017-06-20 10:16:06 +0200

    Mrode

--- Example_Mrode_BLUP.R
+++ Example_Mrode_BLUP.R
@@ -2,38 +2,43 @@ library(pedigree)
 X <- matrix (nrow = 5 , ncol = 2, c(1,0,0,1,1,0,1,1,0,0))
 Z <- matrix (nrow = 5 , ncol = 8, c(rep(0,15), 1, rep(c(rep(0, 5),1),4)))
 y <- as.vector(c(4.5, 2.9, 3.9, 3.5, 5.0))
-
-XtZ = t(X)%*%Z
-ZtX = t(XtZ)
-Xty <- t(X) %*% y
-Zty <- t(Z) %*% y
-ZtZ <- t(Z) %*% Z
-XtX <- t(X) %*% X
-
 ped <- data.frame(id=1:8, dam=c(0,0,0,0,2,2,5,6), sire=c(0,0,0,1,3,1,4,3))
-makeAinv(ped)
-Ai <- read.table("Ainv.txt")
-nInd <- nrow(ped)
-Ainv <- matrix(0,nrow = nInd,ncol = nInd)
-Ainv[as.matrix(Ai[,1:2])] <- Ai[,3]
-dd <- diag(Ainv)
-Ainv <- Ainv + t(Ainv)
-diag(Ainv) <- dd
-
-AinvAlpha <- Ainv*2
-gen <- ZtZ + AinvAlpha
-
-LSEU <- cbind(XtX, XtZ)
-LSEL <- cbind(ZtX, gen)
-LSE <- rbind(LSEU, LSEL)
-yT <- rbind(Xty, Zty)
-
 alpha <- 2
-sol <- solve(LSE) %*% yT
-coeff <- solve(LSE)
 
-diagonal <- diag(coeff)[3:10]
-accuracies <- round(c(sqrt(1 - diagonal*alpha)),3)
+blup <- function(X, Z, y, ped, alpha) {
+  XtZ = t(X)%*%Z
+  ZtX = t(XtZ)
+  Xty <- t(X) %*% y
+  Zty <- t(Z) %*% y
+  ZtZ <- t(Z) %*% Z
+  XtX <- t(X) %*% X
+
+  makeAinv(ped)
+  Ai <- read.table("Ainv.txt")
+  nInd <- nrow(ped)
+  Ainv <- matrix(0,nrow = nInd,ncol = nInd)
+  Ainv[as.matrix(Ai[,1:2])] <- Ai[,3]
+  dd <- diag(Ainv)
+  Ainv <- Ainv + t(Ainv)
+  diag(Ainv) <- dd
+  
+  AinvAlpha <- Ainv*2
+  gen <- ZtZ + AinvAlpha
+  
+  LSEU <- cbind(XtX, XtZ)
+  LSEL <- cbind(ZtX, gen)
+  LSE <- rbind(LSEU, LSEL)
+  yT <- rbind(Xty, Zty)
+
+  
+  sol <- solve(LSE) %*% yT
+  coeff <- solve(LSE)
+  
+  diagonal <- diag(coeff)[3:length(diag(coeff))]
+  accuracies <- round(c(sqrt(1 - diagonal*alpha)),3)
+  return(accuracies)
+}
+
 
 #####################################################################################
 #Mrode example - prepare blupf90 files
@@ -42,48 +47,26 @@ ped <- data.frame(id=1:9, sire=c(0,0,0,1,3,1,4,3,3), dam=c(0,0,0,0,2,2,5,6,2), c
 dat <- data.frame(animal = 1:8, p = c(0,0,0,4.5, 2.9, 3.9, 3.5, 5.0), sex = c(1,2,1,1,2,2,1,1))
 write.table(dat, '/home/jana/Documents/PhD/Mrode.dat', col.names=F, sep=" ", row.names=F, quote=F)
 write.table(ped, '/home/jana/Documents/PhD/Mrode.ped', col.names=F, sep=" ", row.names=F, quote=F)
+#######################################################################################
 
 #add full-sib to animal 5
-ped[9,] <- c(9, 3, 2, 1)
+#ped[9,] <- c(9, 3, 2, 1)
 #female full-sib
-dat[9,] <- c(9, 3.5, 2)
-#male full-sib
-dat[9,] <- c(9, 3.5, 2)
-#what happens to accuracies
+#dat[9,] <- c(9, 3.5, 2)
 
-
-X <- matrix (nrow = 6 , ncol = 2, c(1,0,0,1,1,0,0,1,1,0,0,1))
+ped[9,] <- c(9, 3, 2)
+X <- matrix (nrow = 6 , ncol = 2, c(c(1,0,0,1,1,0),c(0,1,1,0,0,1)))
 Z <- matrix (nrow = 6 , ncol = 9, c(rep(0,18), 1, rep(c(rep(0, 6),1),5)))
 y <- as.vector(c(4.5, 2.9, 3.9, 3.5, 5.0, 3.5))
 
-XtZ = t(X)%*%Z
-ZtX = t(XtZ)
-Xty <- t(X) %*% y
-Zty <- t(Z) %*% y
-ZtZ <- t(Z) %*% Z
-XtX <- t(X) %*% X
+blup(X, Z, y, ped, alpha)
 
-ped <- data.frame(id=1:9, dam=c(0,0,0,0,2,2,5,6,2), sire=c(0,0,0,1,3,1,4,3,3))
-makeAinv(ped)
-Ai <- read.table("Ainv.txt")
-nInd <- nrow(ped)
-Ainv <- matrix(0,nrow = nInd,ncol = nInd)
-Ainv[as.matrix(Ai[,1:2])] <- Ai[,3]
-dd <- diag(Ainv)
-Ainv <- Ainv + t(Ainv)
-diag(Ainv) <- dd
+#add male full-sib
 
-AinvAlpha <- Ainv*2
-gen <- ZtZ + AinvAlpha
+ped[9,] <- c(9, 3, 2)
+X <- matrix (nrow = 6 , ncol = 2, c(c(1,0,0,1,1,1),c(0,1,1,0,0,0)))
+Z <- matrix (nrow = 6 , ncol = 9, c(rep(0,18), 1, rep(c(rep(0, 6),1),5)))
+y <- as.vector(c(4.5, 2.9, 3.9, 3.5, 5.0, 3.5))
 
-LSEU <- cbind(XtX, XtZ)
-LSEL <- cbind(ZtX, gen)
-LSE <- rbind(LSEU, LSEL)
-yT <- rbind(Xty, Zty)
+blup(X, Z, y, ped, alpha)
 
-alpha <- 2
-sol <- solve(LSE) %*% yT
-coeff <- solve(LSE)
-
-diagonal <- diag(coeff)[3:11]
-accuracies <- round(c(sqrt(1 - diagonal*alpha)),3)
