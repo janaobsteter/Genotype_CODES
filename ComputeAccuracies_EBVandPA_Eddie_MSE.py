dde2135d91f0d0a2a4f669ea98355dc5c0ac3d94
janaobsteter <obsteter.jana@gmail.com>
2018-06-13 08:21:46 +0200

    After Portugal

---
+++ ComputeAccuracies_EBVandPA_Eddie_MSE.py
@@ -0,0 +1,61 @@
+from scipy.stats.stats import pearsonr   
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+import math
+import os
+import pandas as pd
+
+WorkingDir = os.getcwd()
+
+
+for scenario in ["Gen"]:
+	for rep in range(0, 21):
+		print(scenario, rep)
+		os.chdir(WorkingDir + '/' +  scenario + str(rep))
+		#os.chdir(AlphaSimDir + 'REAL20GenSel_GenSplosnaPop/')
+		TBVs_all = pd.read_table('SimulatedData/PedigreeAndGeneticValues.txt', sep="\s+").loc[:, ['Generation', 'Indiv', 'Father', 'Mother', 'gvNormUnres1']]
+		accuraciesEBV = pd.DataFrame(columns = ['Rep', 'Cycle', 'Cat', 'mseEBV', 'msePA', 'meanEBV', 'meanTBV', 'meanPA'])
+		ped = pd.read_csv("SimulatedData/PedigreeAndGeneticValues_cat.txt", sep=" ")
+
+		files =	os.listdir(os.getcwd())
+		sol = [i for i in files if i.startswith("renumbered_Solutions_")]
+		number = [int(i.strip("renumbered_Solutions_")) for i in sol]
+
+		for i in range(41,max(number)):
+		    TBVs = TBVs_all.loc[TBVs_all.Generation.isin(range(i-11,i+1))]
+		    accuraciesEBV_temp = pd.DataFrame(columns = ['Rep', 'Cycle', 'Cat', 'mseEBV', 'msePA', 'meanEBV', 'meanTBV', 'meanPA'])
+		    solution = pd.read_table("renumbered_Solutions_" + str(i), header=None, sep="\s+", names = ['renID', 'Indiv', 'EBV', 'Cycle']).loc[:, ['Indiv', 'EBV']]
+		    mseDF = pd.merge(TBVs, solution, on='Indiv')
+		    mseDF.loc[:, 'PA'] = ""
+
+		    catDFEx = 'Categories_gen' + str(i) + 'DF.csv' 
+		    categories = defaultdict(list)
+		    catDF = pd.read_csv(catDFEx)
+		    accuraciesEBV_temp.loc[:, 'Rep'] = [rep] * len(catDF.columns)	
+		    accuraciesEBV_temp.loc[:, 'Cycle'] = [i] * len(catDF.columns)
+		    accuraciesEBV_temp.loc[:,'Cat'] = list(catDF.columns)
+		    accuraciesEBV_temp.loc[:,'YOB'] = [""] * len(catDF.columns) 
+		    for cat in catDF.columns:
+			values = [int(ind) for ind in catDF[cat] if not math.isnan(ind)]
+			categories[cat] = values
+			OneCatDF = mseDF[mseDF.Indiv.isin(categories[cat])]
+			try:
+				accuraciesEBV_temp.loc[(accuraciesEBV_temp.Cycle == i) & (accuraciesEBV_temp.Cat == cat), 'mseEBV'] = np.mean((OneCatDF.gvNormUnres1[OneCatDF.Generation == max(OneCatDF.Generation)] - OneCatDF.EBV[OneCatDF.Generation == max(OneCatDF.Generation)])**2)
+				accuraciesEBV_temp.loc[(accuraciesEBV_temp.Cycle == i) & (accuraciesEBV_temp.Cat == cat), 'meanEBV'] = np.mean(OneCatDF.EBV[OneCatDF.Generation == max(OneCatDF.Generation)])
+				accuraciesEBV_temp.loc[(accuraciesEBV_temp.Cycle == i) & (accuraciesEBV_temp.Cat == cat), 'meanTBV'] = np.mean(OneCatDF.gvNormUnres1[OneCatDF.Generation == max(OneCatDF.Generation)])
+				accuraciesEBV_temp.loc[(accuraciesEBV_temp.Cycle == i) & (accuraciesEBV_temp.Cat == cat), 'YOB'] = max(OneCatDF.Generation)
+			except: 
+				pass
+
+
+
+		    accuraciesEBV = pd.concat([accuraciesEBV, accuraciesEBV_temp])
+		    
+		    
+	accuraciesEBV.loc[:, 'Cycle'] = accuraciesEBV.Cycle.astype(int)
+	accuraciesEBV.to_csv(WorkingDir + '/AccuraciesEBVPA_' + scenario + '.csv', index=None, sep=",")
+
+	
+	print 'Create Data Frame: AccuraciesEBVPA' + scenario + '.csv'
+
--- GA_genotpingHerds2.py
+++ GA_genotpingHerds2.py
@@ -13,6 +13,7 @@ from pyevolve import DBAdapters
 import pandas as pd
 import numpy as np
 
+#define reLu function
 def reLu(number):
     return (0 if number < 0 else number)
     
@@ -21,29 +22,40 @@ def reLu(number):
 #evaluation functions for genetic algorithm
 cowsGen = 5000
 
+#the file with information about relatedness between herds
 HerdsA = pd.read_csv('/home/jana/Documents/PhD/CompBio/RefADF_mean.csv')
+#the file with information about relatedness of herds with test population
 NapA = pd.read_csv('/home/jana/Documents/PhD/CompBio/NapADF_mean.csv')
+##the file with the relatedness of test population with bulls for reference (training population)
 PbA = pd.read_csv('/home/jana/Documents/PhD/CompBio/PbADF_mean.csv')
+#the file containing the number of animals in each herd
 HerdsAnim = pd.read_table("/home/jana/Documents/PhD/CompBio/HerdNo.txt", sep=" ")
 
 
-
+#fitness function
 def eval_func(chromosome):
+    #how many animals from the herds are chosen
     NoAnimals = sum([no for (chrom, no) in zip (chromosome, HerdsAnim.NoAnim) if chrom == 1])
+    #which are the chosen herds (herds 1 - 100)
     chosenHerds = [herd for (chrom, herd) in zip (chromosome, HerdsAnim.Herd) if chrom == 1]
     
+    #calculate the relatedness between the animals within the chosen cow herds for reference population (training population) [list]
     withinA = []
     for index, vals in HerdsA.iterrows():
        if (int(vals.Herd1) in chosenHerds) and (int(vals.Herd2) in chosenHerds): 
            withinA.append(vals.A)
-    
+    #the relatedness of the animals in chosen cow herds with reference bulls [list]
     withPb = (PbA.A[PbA.Herd.isin(chosenHerds)])
+
+    #the relatedness of the chosen cows with animals in the testing population
     withNap = (NapA.A[NapA.Herd.isin(chosenHerds)])
                   
+    #mean relatedness within the reference (training) population
     within = np.mean(list(withPb) + list(withinA))
+    #mean relatedness between the reference and testing population
     between = np.mean (withNap)
 
-    #and also the number of animals 
+    #compute the score considering the relatedness and also the number of animals in the reference
     score = (reLu(between - within) * 10000) **2
     penalty = [-score if (NoAnimals > 1.5*cowsGen or NoAnimals < 0.85*cowsGen) else 0]
     return score+penalty[0]
@@ -51,27 +63,27 @@ def eval_func(chromosome):
     
     
 # Genome instance
-genome = G1DList.G1DList(100)
-genome.setParams(rangemin=0, rangemax=1)
+genome = G1DList.G1DList(100) #chromosome is a list with 100 elements (one for each herds)
+genome.setParams(rangemin=0, rangemax=1) #allowed values are 0 and 1
 
 # The evaluator function (objective function)
-genome.evaluator.set(eval_func)
+genome.evaluator.set(eval_func) #evaluate chromosomes with the chosen fitness function
 #genome.evaluator.add(eval_func2)
-genome.mutator.set(Mutators.G1DListMutatorIntegerBinary)
+genome.mutator.set(Mutators.G1DListMutatorIntegerBinary) #mutate 0 and 1
 
 
 # Genetic Algorithm Instance
 ga = GSimpleGA.GSimpleGA(genome)
-ga.setGenerations(900)
-ga.selector.set(Selectors.GTournamentSelector)
-ga.setMutationRate(0.01)
-ga.setCrossoverRate(0.001)
-ga.setPopulationSize(50)
-ga.terminationCriteria.set(GSimpleGA.ConvergenceCriteria)
+ga.setGenerations(900) #set the number of generations
+ga.selector.set(Selectors.GTournamentSelector) #set the rule for parent selection
+ga.setMutationRate(0.01) #set mutation rate
+ga.setCrossoverRate(0.001) #set cross-over rate
+ga.setPopulationSize(50) #set population size
+#ga.terminationCriteria.set(GSimpleGA.ConvergenceCriteria)
 
 
 # Create DB Adapter and set as adapter
-sqlite_adapter = DBAdapters.DBSQLite(identify="TestHerdsGen")
+sqlite_adapter = DBAdapters.DBSQLite(identify="TestHerdsGen") #this is to write the evolution to a file
 ga.setDBAdapter(sqlite_adapter)
 
 # Using CSV Adapter
