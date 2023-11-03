936a1356c56dd667d6414e531a1a07ae7d975841
janaobsteter <obsteter.jana@gmail.com>
2023-11-03 11:01:08 +0000

    Cleaning up7

--- Copy.sh
+++
@@ -1,5 +0,0 @@
-DIR=/home/jana/Genotipi/Genotipi_CODES
-
-cp $DIR/1_* .
-cp $DIR/Extract800* .
-cp $DIR/GenFiles.py .
--- PlotGenGain_compare.py
+++ PlotGenGain_compare.py
@@ -10,84 +10,39 @@ import os
 
 
 TBV  = TBVPed(os.getcwd() + '/')
-gens, means, vars = TBV.genTrend('/home/jana/bin/AlphaSim1.05Linux/REAL20GenSel_Gen/SimulatedData/PedigreeAndGeneticValues.txt', 41, 60)
-TBV  = TBVPed(os.getcwd() + '/')
-gens1, means1, vars1 = TBV.genTrend('/home/jana/compareSel/PedigreeAndGeneticValues.txt', 41, 60)
-
+gens, means, vars = TBV.genTrend('/home/jana/bin/AlphaSim1.05Linux/REAL20GenSel_Class/SimulatedData/PedigreeAndGeneticValues.txt', 41, 61)
 
 
 TBV  = TBVPed(os.getcwd() + '/')
-gensA, meansA, varsA = TBV.genTrend('/home/jana/bin/AlphaSim1.05Linux/REAL20GenSel_Gen/SimulatedData/PedigreeAndGeneticValues.txt', 41, 6)
+gensA, meansA, varsA = TBV.genTrend('/home/jana/bin/AlphaSim1.05Linux/REAL20GenSel_GenFather/SimulatedData/PedigreeAndGeneticValues.txt', 41, 61)
 
 
 TBV  = TBVPed(os.getcwd() + '/')
-gensG, meansG, varsG = TBV.genTrend('/home/jana/bin/AlphaSim1.05Linux/REAL20GenSel_GenSLO/SimulatedData/PedigreeAndGeneticValues.txt', 41, 61)
+gensG, meansG, varsG = TBV.genTrend('/home/jana/bin/AlphaSim1.05Linux/REAL20GenSel_GenFatherReference/SimulatedData/PedigreeAndGeneticValues.txt', 41, 61)
 #plt.errorbar(x = TBV.gens, y = TBV.means, yerr = TBV.vars)
-
-TBV  = TBVPed(os.getcwd() + '/')
-gensS, meansS, varsS = TBV.genTrend('/home/jana/bin/AlphaSim1.05Linux/REAL20GenSel_GenSplosnaPop/SimulatedData/PedigreeAndGeneticValues.txt', 41, 61)
-
-
-TBV  = TBVPed(os.getcwd() + '/')
-gensD, meansD, varsD = TBV.genTrend('/home/jana/bin/AlphaSim1.05Linux/REAL20GenSel_GenSLO_BmGen/SimulatedData/PedigreeAndGeneticValues.txt', 41, 61)
-#plt.errorbar(x = TBV.gens, y = TBV.means, yerr = TBV.vars)
-TBV  = TBVPed(os.getcwd() + '/')
-gens2, means2, vars2 = TBV.genTrend('/home/jana/bin/AlphaSim1.05Linux//SimulatedData/PedigreeAndGeneticValues.txt', 41, 61)
-#plt.errorbar(x = TBV.gens, y = TBV.means, yerr = TBV.vars)
-
-TBV  = TBVPed(os.getcwd() + '/')
-gensE, meansE, varsE = TBV.genTrend('/home/jana/bin/AlphaSim1.05Linux/REAL20GenSel_Gen/SimulatedData/PedigreeAndGeneticValues.txt', 41, 61)
-#plt.errorbar(x = TBV.gens, y = TBV.means, yerr = TBV.vars)
-
 
 Means = pd.DataFrame()
 Means.loc[:,'Gen'] = [int(x) for x in gens]
 Means.loc[:,'Mean'] = list(means)
 Means.loc[:,'Var'] =  list(vars)
 Means.loc[:,'Stand'] = (Means.Mean - Means.Mean[0]) / sqrt(Means.Var[0])
-
-
-
-Means.loc[:,'Gen1'] = [int(x) for x in gens1]
-Means.loc[:,'Mean1'] = list(means1)
-Means.loc[:,'Var1'] =  list(vars1)
-Means.loc[:,'Stand1'] = (Means.Mean1 - Means.Mean1[0]) / sqrt(Means.Var1[0])
-
-
+Means.loc[:,'GenG'] = [int(x) for x in gensG]
+Means.loc[:,'MeanG'] = list(meansG)
+Means.loc[:,'VarG'] =  list(varsG)
+Means.loc[:,'StandG'] = (Means.MeanG - Means.MeanG[0]) / sqrt(Means.VarG[0])
 Means.loc[:,'GenA'] = [int(x) for x in gensA]
 Means.loc[:,'MeanA'] = list(meansA)
 Means.loc[:,'VarA'] =  list(varsA)
 Means.loc[:,'StandA'] = (Means.MeanA - Means.MeanA[0]) / sqrt(Means.VarA[0])
 
-Means.loc[:,'GenG'] = [int(x) for x in gensG]
-Means.loc[:,'MeanG'] = list(meansG)
-Means.loc[:,'VarG'] =  list(varsG)
-Means.loc[:,'StandG'] = (Means.MeanG - Means.MeanG[0]) / sqrt(Means.VarG[0])
 
 
-Means.loc[:,'GenS'] = [int(x) for x in gensS]
-Means.loc[:,'MeanS'] = list(meansS)
-Means.loc[:,'VarS'] =  list(varsS)
-Means.loc[:,'StandS'] = (Means.MeanS - Means.MeanS[0]) / sqrt(Means.VarS[0])
+plt.plot( Means.Gen, Means.Stand,  label = 'Mean Gen TBV_class')
 
-Means.loc[:,'GenD'] = [int(x) for x in gensD]
-Means.loc[:,'MeanD'] = list(meansD)
-Means.loc[:,'VarD'] =  list(varsD)
-Means.loc[:,'StandD'] = (Means.MeanD - Means.MeanD[0]) / sqrt(Means.VarD[0])
+plt.plot( Means.GenA, Means.StandA,  label = 'Mean Gen TBV_genFather')
 
-
-
-
-
-plt.plot( Means.Gen, Means.Stand,  label = 'Conventional')
-plt.plot( Means.Gen1, Means.Stand1,  label = 'genEddie')
-plt.plot( Means.GenG, Means.StandG,  label = 'Conventional_SLO')
-plt.plot( Means.GenS, Means.StandS,  label = 'GenBulls on Other Cows')
-plt.plot( Means.GenD, Means.StandD,  label = 'GenBulls on Bull Dams')
-plt.plot( Means.GenA, Means.StandA,  label = 'GenBulls on All Cows')
-plt.plot( Means.GenE, Means.StandE,  label = 'GenBulls on All Cows1')
+plt.plot( Means.GenG, Means.StandG,  label = 'Mean Gen TBV_genFatherRef')
 plt.xticks(Means.Gen)
-
 plt.xlabel('Selected Generation')
 plt.ylabel('Mean Generation TBV')
 legend(loc='upper left')
@@ -105,25 +60,10 @@ meansEBV = EBVs.EBV.groupby(EBVs.Generation).aggregate(np.mean)
 EBV = TBVPed(AlphaSimDir)
 gensE, meansE, varsE = EBV.genTrend_EBV('/home/jana/bin/AlphaSim1.05Linux/REAL20GenSel_Class/EBVs.txt', 1, 60)
 
-plt.plot(gens, meansEBV, label='EBV')
+plt.plot(gensEBV, meansEBV, label='EBV')
 plt.xlabel('Selected Generation')
 plt.ylabel('Mean Generation TBV')
 legend(loc='upper left')
-
-
-plt.plot(gens, means, label='Conventional')
-plt.plot(gens1, means1, label='Conventional_rep1')
-plt.plot(gensG, meansG, label='Conventional_SLO')
-plt.plot(gensS, meansS, label='GenBulls on Other Cows')
-plt.plot(gensD, meansD, label='GenBulls on Bull Dams')
-plt.plot(gensE, meansE, label='GenBulls on all cows')
-plt.plot(gens2, means2, label='GenBulls on Bull Dams2')
-
-#plt.plot(gensA, meansA, label='GenBulls on all cows BitWrong')
-plt.xlabel('Selected Generation')
-plt.ylabel('Mean Generation TBV')
-legend(loc='upper left')
-
 
 
 
